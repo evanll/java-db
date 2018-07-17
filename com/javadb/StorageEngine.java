bed00694a23e8e3c15bc4dffb5d756ee138ccfaa
evanlal <el17604@my.bristol.ac.uk>
2018-07-17 12:28:17 +0300

    copyright notice update

--- com/javadb/StorageEngine.java
+++ com/javadb/StorageEngine.java
@@ -1,3 +1,9 @@
+/*
+ * Written by Evan Lalopoulos <evan.lalopoulos.2017@my.bristol.ac.uk>
+ * Copyright (C) 2018 - All rights reserved.
+ * Unauthorized copying of this file is strictly prohibited.
+ */
+
 package com.javadb;
 
 import java.io.*;
